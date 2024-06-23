3e3873679bcf6c196e6502e2ab60cfef1b1cd016
Tomarovsky <andrey.tomarovsky@gmail.com>
2024-06-23 08:16:26 +0300

    fix

--- config/nvim/lua/pydev/plugins/nvim-tree.lua
+++ config/nvim/lua/pydev/plugins/nvim-tree.lua
@@ -37,9 +37,25 @@ return {
       },
     })
 
+    vim.api.nvim_create_autocmd("QuitPre", {
+      callback = function()
+        local invalid_win = {}
+        local wins = vim.api.nvim_list_wins()
+        for _, w in ipairs(wins) do
+          local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+          if bufname:match("NvimTree_") ~= nil then
+            table.insert(invalid_win, w)
+          end
+        end
+        if #invalid_win == #wins - 1 then
+          -- Should quit, so we close all invalid windows.
+          for _, w in ipairs(invalid_win) do vim.api.nvim_win_close(w, true) end
+        end
+      end
+    })
+
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
-
     keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
     keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
